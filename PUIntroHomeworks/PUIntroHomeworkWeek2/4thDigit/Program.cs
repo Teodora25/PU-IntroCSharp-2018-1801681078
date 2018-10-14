f4eb54d918185b6e5938e2b230fbe8496048f639
Teodora25 <teodora_225@abv.bg>
2018-10-14 12:53:38 +0300

    included homeworks for week 2

---
+++ PUIntroHomeworks/PUIntroHomeworkWeek2/4thDigit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _4thDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a 7-digit number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            // Check if the input number has 7 digits
+            if (number < 1000000 || number > 9999999)
+            {
+                // If it hasn't, display a suitable message
+                Console.WriteLine("The number you entered is not a 7-digit number!");
+            }
+            else
+            {
+                /* If it has, find the fourth digit. You can search the Internet
+                   for another and better algorithm for finding the 4th digit. */
+                int fourthDigit = number % 1000000 % 100000 % 10000 / 1000;
+
+                Console.WriteLine($"The 4th digit of {number} is {fourthDigit}");
+            }
+        }
+    }
+}
---
+++ PUIntroHomeworks/PUIntroHomeworkWeek2/TravelingToSunWithSoundSpeed/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TravelingToSunWithSoundSpeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* The values of the constants in this task are not important.
+               The algorithm is what matters. */
+            const double speedOfSound = 1225.044; // in km/h
+            const long distToSun = 149600000; // in km
+            double timeNeeded = Math.Round(distToSun / speedOfSound); // in hours
+            // Search in the Internet how to transform hours into years and days.
+            double timeNeededInYears = Math.Floor(timeNeeded / 8760);
+            double leftoverDays = Math.Floor(((timeNeeded / 8760) - timeNeededInYears) * 365);
+
+            Console.WriteLine($"The time needed to travel from the Earth to the Sun is {timeNeeded} hours, or " +
+$"{timeNeededInYears} years and {leftoverDays} days.");
+        }
+    }
+}
